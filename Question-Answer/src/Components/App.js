f483be7c9b9ff3e34647e95d4f0f27fbe3eefeed
Ardhendu <ardhendujj002@gmail.com>
2021-07-16 12:21:42 +0530

    Added another project of react

---
+++ Question-Answer/src/Components/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import data from "./data";
+import SingleQuestion from "./Question";
+function App() {
+  return (
+    <main>
+      <div className="container">
+        <h3>questions and answers about login</h3>
+        <section className="info">
+          {data.map((question) => {
+            return (
+              <SingleQuestion key={question.id} {...question}></SingleQuestion>
+            );
+          })}
+        </section>
+      </div>
+    </main>
+  );
+}
+
+export default App;
